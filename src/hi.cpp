3298408857b9122369332b504f0105225ff39ee7
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-12-09 19:06:21 -0500

    christ on a stick

--- src/hi.cpp
+++ src/hi.cpp
@@ -1,4 +1,3 @@
-#define GEODE_EXPOSE_SECRET_INTERNALS_IN_HEADERS_DO_NOT_DEFINE_PLEASE
 #include <Geode/Geode.hpp>
 #include <Geode/ui/GeodeUI.hpp>
 
@@ -19,7 +18,7 @@ class $modify(PauseLayerExt, PauseLayer) {
             }
             if (auto item = CCMenuItemExt::createSpriteExtraWithFrameName(
                 "GJ_chatBtn_001.png", 0.6f, [](void*) {
-                    openInfoPopup(getMod());
+                    openInfoPopup(Mod::get());
                 }
             )) {
                 auto sprite = item->getNormalImage();
@@ -44,29 +43,30 @@ class $modify(PlayLayerExt, PlayLayer) {
         void reload(float) {
 			if (!this or !typeinfo_cast<InfoLayer*>(this)) return;
             //dont reset when dragging
-            if (auto a = m_list) if (auto b = a->m_list)
-                if (auto c = b->m_tableView) if (c->m_touchDown) return;
+            if (m_list) if (m_list->m_list)
+                if (m_list->m_list->m_tableView)
+                    if (m_list->m_list->m_tableView->m_touchDown) return;
             //reset
-            this->onRefreshComments(0);
+            this->onRefreshComments(nullptr);
         }
         void setup(float) {
 			if (!this or !typeinfo_cast<InfoLayer*>(this)) return;
             this->setPosition(
-                getMod()->getSavedValue("window.pos.x", -202.000),
-                getMod()->getSavedValue("window.pos.y", -49.000f)
+                Mod::get()->getSavedValue<float>("window.pos.x", -202.000),
+                Mod::get()->getSavedValue<float>("window.pos.y", -49.000f)
             );
-            this->setScale(getMod()->getSettingValue<float>("scale"));
+            this->setScale(Mod::get()->getSettingValue<float>("scale"));
             this->setOpacity(0);
             this->setZOrder(999);
             for (auto a : m_mainLayer->getChildrenExt()) a->setVisible(
                 typeinfo_cast<GJCommentListLayer*>(a)
             );
-            m_reportBtn->setVisible(0);
-            if (auto a = m_timeBtn) if (auto p = a->getParent()) p->setVisible(
-                getMod()->getSettingValue<bool>("show-left-side-menu")
+            m_reportBtn->setVisible(false);
+            if (m_timeBtn) if (auto p = m_timeBtn->getParent()) p->setVisible(
+                Mod::get()->getSettingValue<bool>("show-left-side-menu")
             );
-            if (auto a = m_buttonMenu) a->setVisible(
-                getMod()->getSettingValue<bool>("show-menu")
+            if (m_buttonMenu) m_buttonMenu->setVisible(
+                Mod::get()->getSettingValue<bool>("show-menu")
             );
             if (m_list) {
                 for (auto a : m_list->getChildrenExt()) a->setVisible(
@@ -74,12 +74,12 @@ class $modify(PlayLayerExt, PlayLayer) {
                 );
                 findFirstChildRecursive<CCLayerColor>(
                     m_list, [](CCLayerColor* node) {
-                        node->setColor(getMod()->getSettingValue<ccColor3B>("color"));
-                        node->setOpacity(getMod()->getSettingValue<int>("opacity"));
+                        node->setColor(Mod::get()->getSettingValue<ccColor3B>("color"));
+                        node->setOpacity(Mod::get()->getSettingValue<int>("opacity"));
                         return false;
                     }
                 );
-                if (auto a = m_list->m_list) if (auto scroll = a->m_tableView) {
+                if (m_list->m_list) if (auto scroll = m_list->m_list->m_tableView) {
                     scroll->setMouseEnabled(false);
                     scroll->m_disableVertical = false;
                     scroll->m_disableHorizontal = false;
@@ -92,8 +92,8 @@ class $modify(PlayLayerExt, PlayLayer) {
                             m_reportBtn->getPosition(),
                             content->getPosition()
                         );
-                        getMod()->setSavedValue("window.pos.x", offset.x);
-                        getMod()->setSavedValue("window.pos.y", offset.y);
+                        Mod::get()->setSavedValue<float>("window.pos.x", offset.x);
+                        Mod::get()->setSavedValue<float>("window.pos.y", offset.y);
                         content->setPosition(m_reportBtn->getPosition());
                     }
                 }
@@ -101,27 +101,25 @@ class $modify(PlayLayerExt, PlayLayer) {
         }
     };
     void updateInfoLayer(float) { //meant to be called on scene
-        if (auto info = getChildByType<InfoLayer>(0)) {
-            info->scheduleOnce(schedule_selector(InfoLayerUpdater::setup), 0.f);
+        if (auto info = m_uiLayer->getChildByID("comment-widget"_spr)) {
+            static_cast<InfoLayer*>(info)->scheduleOnce(schedule_selector(InfoLayerUpdater::setup), 0.f);
 		}
 	}
     void updateInfoLayerLive(float) { //meant to be called on scene
-		if (auto info = getChildByType<InfoLayer>(0)) {
-            info->scheduleOnce(schedule_selector(InfoLayerUpdater::reload), 0.f);
+		if (auto info = m_uiLayer->getChildByID("comment-widget"_spr)) {
+            static_cast<InfoLayer*>(info)->scheduleOnce(schedule_selector(InfoLayerUpdater::reload), 0.f);
 		}
 	}
-    virtual void onEnterTransitionDidFinish() {
-		PlayLayer::onEnterTransitionDidFinish();
-        if (!m_level) return;
-        if (!getParent()) return;
-        auto sc = getParent();
-        auto info = InfoLayer::create(m_level, 0, 0);
+    virtual void setupHasCompleted() {
+		PlayLayer::setupHasCompleted();
+        if (!m_level || !m_uiLayer) return;
+        auto info = InfoLayer::create(m_level, nullptr, nullptr);
         info->setMouseEnabled(false);
         info->setTouchEnabled(false);
         info->setKeyboardEnabled(false);
-        handleTouchPriorityWith(info, 0, true);
-        sc->addChild(info);
-        sc->schedule(schedule_selector(PlayLayerExt::updateInfoLayer));
-        sc->schedule(schedule_selector(PlayLayerExt::updateInfoLayerLive), 1.f);
+        info->setID("comment-widget"_spr);
+        m_uiLayer->addChild(info);
+        this->schedule(schedule_selector(PlayLayerExt::updateInfoLayer));
+        this->schedule(schedule_selector(PlayLayerExt::updateInfoLayerLive), 5.f);
     };
 };
